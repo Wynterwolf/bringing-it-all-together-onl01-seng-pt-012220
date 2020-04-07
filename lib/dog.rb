b917bc11226d0cdb3924596213033b0d82a54f4f
Samantha Jordan <shinyguns@gmail.com>
2020-04-07 19:22:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,5 +2,14 @@ class Dog
 
 attr_accessor :id, :name, :breed 
 
+#initialize method 
+def initialize (id=null, name, breed)
+  @id = id
+  @name = name
+  @breed = breed
+end
+
+#create table 
+def self.create_table #class method
 
 end
